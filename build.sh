891c19b22608c11521781db18a18139fd3765853
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-07-26 16:23:27 +1000

    increased build memory

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 mkdir output
 rm countdown.love
 zip -9 -r countdown.love .
-npx love.js -c countdown.love output
+npx love.js -c countdown.love output -m 106777216
 cd output
 python -m http.server 8000

7192723bd282247383769e493bad0b176d2ff91d
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-08-21 23:30:53 +1000

    fix build script

--- build.sh
+++ build.sh
@@ -5,7 +5,14 @@ sh asepritecompile.sh
 cd ..
 mkdir output
 rm countdown.love
-zip -9 -r countdown.love .
+FILES=""
+
+FILES="$FILES $(find -type f -name "*.png")"
+FILES="$FILES $(find -type f -name "*.lua")"
+FILES="$FILES $(find -type f -name "*.ogg")"
+FILES="$FILES $(find -type f -name "*.wav")"
+FILES="$FILES $(find -type f -name "*.otf")"
+zip -9 -r countdown.love $FILES
 npx love.js -c countdown.love output -m 106777216
 cd output
 python -m http.server 8000
